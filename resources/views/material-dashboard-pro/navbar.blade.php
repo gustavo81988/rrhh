0a3b5a0c68687689b3b33441960dc1bda2958dd8
Gustavo Ramírez <gustavo81988@gmail.com>
2019-02-08 15:36:46 -0500

    avance

--- resources/views/material-dashboard-pro/navbar.blade.php
+++ resources/views/material-dashboard-pro/navbar.blade.php
@@ -20,7 +20,7 @@
       <form class="navbar-form" method="POST" action="{{route('company.select')}}">
         @csrf
         <div class="input-group no-border">
-            <select onChange="this.form.submit()" class="selectpicker" data-style="select-with-transition"  title="Choose City" data-size="7">
+            <select onChange="this.form.submit()" class="selectpicker" data-style="select-with-transition">
                 <option selected disabled value="">Empresa</option>
                 @foreach (Auth::user()->companies as $key => $value)
                     <option {{(Auth::user()->company == $key) ? 'selected' : ''}} value="{{$key}}">
